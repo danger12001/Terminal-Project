18adb1039341e735d1b3b6df9235013aec450b25
danger12001 <paul@projectcodex.co>
2016-04-10 21:35:32 +0200

    V1.01

--- js/script.js
+++ js/script.js
@@ -3,13 +3,27 @@ var userInput = document.getElementById("userInput");
 var userInputs = document.querySelector("input");
 var AiOutput = document.getElementById("AI-output");
 
-var Prompt2 = ">" +"Hello " + userName + ", Please Answer 'Yes' or 'No': INSERT QUESTION HERE";
+var Prompt2 = ">" +"Hello " + userName + ", INSERT QUESTION HERE, type: either 'yes' or 'no' to continue";
 var Prompt3 = ">" + "INSERT NEXT QUESTION HERE";
+var Prompt4 = ">" + "FINAL EXAMPLE QUESTION";
+var Prompt5 = ">" + "Splooof";
 AiOutput.innerHTML = Prompt2;
 userInputs.onkeydown = function(e){
    if (e.keyCode === 13) {
-     if(userInput.value == ">yes"){
+     if(userInput.value == ">" + "yes"){
        AiOutput.innerHTML = Prompt3;
+     }
+     userInput.value = ">";
+   }
+   if (e.keyCode === 13) {
+     if(userInput.value == ">" + "yes"){
+       AiOutput.innerHTML = Prompt4;
+     }
+     userInput.value = ">";
+   }
+   if (e.keyCode === 13) {
+     if(userInput.value == ">" + "yes"){
+       AiOutput.innerHTML = Prompt5;
      }
      userInput.value = ">";
    }
